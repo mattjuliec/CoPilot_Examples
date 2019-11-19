5a1ca2baa897d5f985e47a1604a839ca2de8d5e1
Matthew A Clark <mattclark@galois.com>
2019-11-19 13:01:37 -0500

    Updates with documentation

--- DOUBLEIT/doubleIT.c
+++ DOUBLEIT/doubleIT.c
@@ -4,10 +4,14 @@
 //  > ./monitor
 //  > c99 doubleIT.c monitor.c -o doubleIT
 
+//include the copilot monitor header file
 #include "monitor.h"
 
+// initialize the global variable accessed by the monitor
 double value = 0.0;
 
+// define the functions that are triggered if the monitor sees a value that violates the 
+// specification
 void too_low(double too_low_arg0)
 {
      value = 0.0;
@@ -17,6 +21,8 @@ void too_high(double too_high_arg0)
      value = 200.0;
 }
 
+// read in the value from the simulation, double it, and then check to see if the 
+// monitor is triggered.
 double doubleIT(double u)
 {
      value = u*2;
